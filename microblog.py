2983954c1930310b34d4530b7c1eb67cca889eec
burrito-mode <jpantsniner@gmail.com>
2021-10-07 12:10:00 -0600

    verbose commenting on shell context

--- microblog.py
+++ microblog.py
@@ -2,6 +2,8 @@ from app import app, db
 from app.models import User, Post
 
 
+# for testing in the shell interpreter
+# all imports and db will be available using >>$ flask shell
 @app.shell_context_processor
 def make_shell_context():
     return {'db': db, 'User': User, 'Post': Post}

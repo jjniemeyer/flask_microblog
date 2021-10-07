6cfcb6c04567aa6fad8ee008be52f1731adfc387
burrito-mode <jpantsniner@gmail.com>
2021-10-07 10:49:43 -0600

    init db

--- config.py
+++ config.py
@@ -1,7 +1,10 @@
 import os
-
-# storing config items are stored in a class variable for extensibility
+basedir = os.path.abspath(os.path.dirname(__file__))
+# config items are stored in a class variable for extensibility
 class Config(object):
     # placeholder key for dev, prefer environment variable,
     # update to real key later
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+                              'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False

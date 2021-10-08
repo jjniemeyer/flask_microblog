630a0927a9b20c44b625b2b43c47fd8d282700c7
burrito-mode <jpantsniner@gmail.com>
2021-10-07 22:16:18 -0600

    implement user registration

--- app/routes.py
+++ app/routes.py
@@ -5,6 +5,8 @@ from flask_login import current_user, login_user
 from app.models import User
 from flask_login import logout_user, login_required
 from werkzeug.urls import url_parse
+from app import db
+from app.forms import RegistrationForm
 
 @app.route('/')
 @app.route('/index')
@@ -29,7 +31,7 @@ def index():
         }
 
     ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    return render_template('index.html', title='Home',  posts=posts)
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -54,3 +56,17 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)

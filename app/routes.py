071f50567bad15585a3d6c223be9aff89e31196f
burrito-mode <jpantsniner@gmail.com>
2021-10-09 14:11:54 -0600

    implement last_seen

--- app/routes.py
+++ app/routes.py
@@ -7,6 +7,14 @@ from flask_login import logout_user, login_required
 from werkzeug.urls import url_parse
 from app import db
 from app.forms import RegistrationForm
+from datetime import datetime
+
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
 
 @app.route('/')
 @app.route('/index')

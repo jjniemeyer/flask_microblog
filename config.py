d7fe3f18f7078de4a2617b54ea83aaa6f4a61175
burrito-mode <jpantsniner@gmail.com>
2021-10-12 12:42:53 -0600

    basic pagination

--- config.py
+++ config.py
@@ -16,3 +16,6 @@ class Config(object):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['jani5714@colorado.edu']
+
+    #config for pagination, LIMIT POSTS TO 3 for testing, more later
+    POSTS_PER_PAGE = 3

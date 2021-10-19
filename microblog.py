841ed1450a7f702b40b5a5f491e15ced395ea583
burrito-mode <jpantsniner@gmail.com>
2021-10-18 21:57:44 -0600

    complete refactor dump

--- microblog.py
+++ microblog.py
@@ -1,9 +1,10 @@
-from app import app, db, cli
+from app import create_app, db, cli
 from app.models import User, Post
 
+app = create_app()
+cli.register(app)
 
-# for testing in the shell interpreter
-# all imports and db will be available using >>$ flask shell
+
 @app.shell_context_processor
 def make_shell_context():
     return {'db': db, 'User': User, 'Post': Post}
